56b4e310ea97bb54c933842757b3b8cee8557be4
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-17 13:02:44 +0800

    feat: added routing.

---
+++ src/routes/paths.ts
@@ -0,0 +1,5 @@
+export const paths = {
+    home: "/",
+    product: "/product",
+    productDetail: "/product/:id",
+}
---
+++ src/routes/routes.tsx
@@ -0,0 +1,24 @@
+import {
+    Navigate,
+    RouteObject
+} from "react-router";
+
+import { paths } from "./paths";
+
+import Product from "../modules/products/Product";
+import ProductDetail from "../modules/product-details/ProductDetail";
+
+export const routes: RouteObject[] = [
+    {
+        path: paths.home,
+        element: <Navigate to={paths.product} />,
+    },
+    {
+        path: paths.product,
+        element: <Product />,
+    },
+    {
+        path: `${paths.product}/:id`,
+        element: <ProductDetail />,
+    }
+]
