8cc446a5e4b4a3a2a149674041f1b3a280bec6cf
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-17 13:23:39 +0800

    feat: used styles in product.tsx

--- src/modules/products/Product.tsx
+++ src/modules/products/Product.tsx
@@ -1,7 +1,16 @@
+import { Box, Typography, useTheme } from "@mui/material";
+
 export default function Product() {
+    const theme = useTheme();
+
     return (
-        <div>
-            <h1>Product</h1>
-        </div>
+       <Box sx={{ p:2, bgcolor: 'background.paper' }}>
+            <Typography variant="h5" sx={{ color: 'primary.main', mb: 2 }}>
+                Product List
+            </Typography>
+            <Typography variant="h5" sx={{ color: theme.colors.secondary, mb: 2 }}>
+                This is the product list page.
+            </Typography>
+       </Box>
     )
 }
