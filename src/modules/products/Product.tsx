704dc4b4bffa4e98654e5ffc237586270323b540
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-18 19:58:40 +0800

    feat: implemented store in product page.

--- src/modules/products/Product.tsx
+++ src/modules/products/Product.tsx
@@ -1,16 +1,28 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box, Button, Typography, useTheme } from "@mui/material";
 
 import useProductsStore from "./store";
 
 export default function Product() {
     const theme = useTheme();
-    const { products } = useProductsStore();
+    const { products, addProduct } = useProductsStore();
 
     return (
        <Box sx={{ p:2, bgcolor: 'background.paper' }}>
             <Typography variant="h5" sx={{ color: 'primary.main', mb: 2 }}>
-                Product List
+                Product List {products.length}
             </Typography>
+
+            {products.map((p) => (
+                <Typography key={p.id}>
+                    #{p.id} — {p.name}
+                </Typography>
+            ))}
+
+            <Button variant="contained" 
+                    color="primary" 
+                    onClick={() => addProduct({ id: products.length + 1, name: `Product ${products.length + 1}` })}>
+                Add Product
+            </Button>
             <Typography variant="h5" sx={{ color: theme.colors.secondary, mb: 2 }}>
                 This is the product list page.
             </Typography>
