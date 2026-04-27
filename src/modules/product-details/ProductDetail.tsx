439e5581d1f5c1f79e6baf3c8c90e57d62b74e57
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-27 22:17:46 +0800

    feat: added breadcrumpts.

--- src/modules/product-details/ProductDetail.tsx
+++ src/modules/product-details/ProductDetail.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
+import { generatePath, useParams } from 'react-router-dom';
+import paths from '../../routes/paths'
 
 import useProductDetailStore from './store';
 
 export default function ProductDetail() {
     const getProduct = useProductDetailStore((s) => s.getProduct);
+    const { productId } = useParams<{ productId: string }>();
 
     useEffect(() => {
         getProduct(
@@ -11,8 +14,13 @@ export default function ProductDetail() {
         );
     }, [getProduct]);
 
+    const productDetailsPath = generatePath(paths.productDetail, {
+        productId,
+    });
+
     return (
         <div>
+            <p>{paths.home}/{paths.product}/{paths.productDetail}</p>
             <h1>Product Detail </h1>
         </div>
     )
